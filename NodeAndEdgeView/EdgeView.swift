93d27df1d4a6f5edf91b72e1d0feadddc4000157
Taro <taro@taronomacbook-pro.local>
2019-01-22 21:29:48 +0900

    Edgeを表示するための具体的なレイヤーを用意 色や線の太さは用意調整

---
+++ NodeAndEdgeView/EdgeView.swift
@@ -0,0 +1,40 @@
+//
+//  EdgeView.swift
+//  NodeAndEdgeView
+//
+//  Created by Taro on 2019/01/22.
+//  Copyright © 2019 Taro. All rights reserved.
+//
+
+import UIKit
+
+class EdgeView:CAShapeLayer{
+    private weak var parentNodeView:NodeView?
+    private weak var childNodeView:NodeView?
+    
+    init(parentNodeView:NodeView?, childNodeView:NodeView?) {
+        self.parentNodeView = parentNodeView
+        self.childNodeView = childNodeView
+        super.init()
+        self.drawEdge()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func drawEdge(){
+        guard let unwrappedParentNodeView = self.parentNodeView, let unwrappedChildNodeView = self.childNodeView else {
+            print("Edge drawing failed")
+            return
+        }
+        
+        let edge = UIBezierPath()
+        edge.move(to: unwrappedParentNodeView.center)
+        edge.addLine(to: unwrappedChildNodeView.center)
+        edge.close()
+        self.lineWidth = 15.0
+        self.strokeColor = UIColor.orange.cgColor
+        self.path = edge.cgPath
+    }
+}
