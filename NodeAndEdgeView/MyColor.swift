768ed6dfebe328cfadf9aa5002a5f808aa0026f4
Taro <taro@taronomacbook-pro.local>
2019-03-22 16:06:32 +0900

    Create original color.

---
+++ NodeAndEdgeView/MyColor.swift
@@ -0,0 +1,23 @@
+//
+//  MyColor.swift
+//  NodeAndEdgeView
+//
+//  Created by Taro on 2019/03/22.
+//  Copyright © 2019 Taro. All rights reserved.
+//
+
+import UIKit
+
+class MyColor:UIColor {
+    class var myBlue:UIColor{
+        return UIColor(red: 25.0/255.0, green: 70.0/255.0, blue: 89.0/255.0, alpha: 1.0)
+    }
+    
+    class var myLightBlue:UIColor{
+        return UIColor(red: 19.0/255.0, green: 107.0/255.0, blue: 126.0/255.0, alpha: 1.0)
+    }
+    
+    class var beige:UIColor{
+        return UIColor(red: 255.0/255.0, green: 249.0/255.0, blue: 223.0/255.0, alpha: 1.00)
+    }
+}
